b91a32e0fe650bbdac45947d0d960a60d7b74c47
yanjigb <yanji.notification@gmail.com>
2024-10-18 10:13:53 +0700

    feat(feature): :sparkles: [#7] Add Format Date

--- src/app/lib/scheduler-sync-new-payment.js
+++ src/app/lib/scheduler-sync-new-payment.js
@@ -1,14 +1,15 @@
 const cron = require("node-cron");
 const { paymentCassoService } = require("../services/payment-casso.service");
 const ColorConsole = require("./color-console");
+const { formatDateTime } = require("../utils/format-date");
 const RUN_EVERY_1_MINUTE = "*/1 * * * *";
 
 cron.schedule(RUN_EVERY_1_MINUTE, async () => {
   try {
-    ColorConsole("info", "Bắt đầu kiểm tra giao dịch mới...");
+    ColorConsole("info", `Bắt đầu kiểm tra giao dịch mới...              ${formatDateTime(new Date())}`);
     await paymentCassoService.handleUserPaid();
-    ColorConsole("success", "Đã kiểm tra và cập nhật số dư người dùng.");
+    ColorConsole("success", `Đã kiểm tra và cập nhật số dư người dùng.   ${formatDateTime(new Date())}`);
   } catch (err) {
-    ColorConsole("error", `Error schedule sync new transfer: ${err}`);
+    ColorConsole("error", `Error schedule sync new transfer: ${err}      ${formatDateTime(new Date())}`);
   }
 });
